a7c8eb430b360b70efcd15c4d5057643b7732cfe
unknown <BosUssur@gmail.com>
2021-11-29 23:55:57 +0300

    Added two scasses to Leson 1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte totalCores = 4;
+        short baseFrequency = 3500;
+        int memorySize = 128;
+        long tdp = 95;
+        float voltage = 220;
+        double price = 450.99;
+        char os = 'W';
+        boolean server = true;
+
+        System.out.println(totalCores);
+        System.out.println(baseFrequency);
+        System.out.println(memorySize);
+        System.out.println(tdp);
+        System.out.println(voltage);
+        System.out.println(price);
+        System.out.println(os);
+        System.out.println(server);
+    }
+}
