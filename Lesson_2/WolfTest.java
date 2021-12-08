f6b65c8377b18bb628bfcd01ee6e2e953c582dce
MaksimBS <95247330+MaksimBS@users.noreply.github.com>
2021-12-08 08:23:52 +0300

    Update WolfTest.java

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,19 +1,23 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf test = new Wolf();
+        // проверка пола
         test.setGenderMan(true);
         if (test.isGenderMan()) {
             System.out.println("Пол волка = Мужской");
         } else {
             System.out.println("Пол волка = Женский");
         }
+        // Провека клички
         test.setNickName("Серый");
         System.out.println("Кличка волка = " + test.getNickName());
+        // Проверка возраста
         test.setAge(11);
         System.out.println("Возраст волка = " +  test.getAge());
+        // Проверка цвета
         test.setСolor("Белый");
         System.out.println("Цвет волка = " + test.getСolor());
-
+        
         test.move();
         test.sit();
         test.run();
