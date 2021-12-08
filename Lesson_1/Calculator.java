2a2fd3505a91e630034045bcb8ef991de3978c33
Maksim <95247330+MaksimBS@users.noreply.github.com>
2021-12-08 14:56:06 +0300

    Очередное исправление, пробелы и result *= num1
    исправлено имя переменной symbol -> mathSign
    добавлены пробелы до и после операторов
    исправлены операторы *=

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,46 +4,38 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Калькурятор");
         System.out.println("Вводим число 1, вводим знак (+, -, *, /, ^, %), вводим 2 число");
-        System.out.println("");
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Введите целое чилсо 1: ");
+        System.out.print("\nВведите целое чилсо 1: ");
         int num1 = sc.nextInt();
         // This line you have to add (It consumes the \n character)
         sc.nextLine();
 
         System.out.print("Введие знак: ");
-        String symbol = sc.nextLine();
+        String mathSign = sc.nextLine();
 
         System.out.print("Введите целое чилсо 2: ");
         int num2 = sc.nextInt();
 
-        //Сложение
-        if (symbol.equals("+")) {
-            System.out.println(num1+symbol+num2+"="+(num1+num2));
-            //Вычетание
-        } else if (symbol.equals("-")) {
-            System.out.println(num1+symbol+num2+"="+(num1-num2));
-            //Умножение
-        } else if (symbol.equals("*")) {
-            System.out.println(num1+symbol+num2+"="+(num1*num2));
-            //Диление
-        } else if (symbol.equals("/")) {
-            System.out.println(num1+symbol+num2+"="+(num1/num2));
-            //Возведение в степень
-        } else if (symbol.equals("^")) {
+        if (mathSign.equals("+")) {
+            System.out.println(num1 + mathSign + num2 + "=" + (num1 + num2));
+        } else if (mathSign.equals("-")) {
+            System.out.println(num1 + mathSign + num2 + "=" + (num1 - num2));
+        } else if (mathSign.equals("*")) {
+            System.out.println(num1 + mathSign + num2 + "=" + (num1 * num2));
+        } else if (mathSign.equals("/")) {
+            System.out.println(num1 + mathSign + num2 + "=" + (num1 / num2));
+        } else if (mathSign.equals("^")) {
             int result = 1;
-            for (int i = 1; i < num2+1; i++) {
-                result = result * num1;
+            for (int i = 1; i < num2 + 1; i++) {
+                result *= num1;
             }
-            System.out.println(num1+symbol+num2+"="+result);
-            //Остаток от деления
-        } else if (symbol.equals("%")) {
+            System.out.println(num1 + mathSign + num2 + "=" + result);
+        } else if (mathSign.equals("%")) {
             double x;
-            x = (double)num1 / (double)num2;
-            System.out.println(num1+symbol+num2+"="+ (x % 1));
-            // если ввели неверный знак, предупреждение
+            x = (double) num1 / (double) num2;
+            System.out.println(num1 + mathSign + num2 + "=" + (x % 1));
         } else {
             System.out.println("Ошибка. Ввели неверные данные!");
         }
