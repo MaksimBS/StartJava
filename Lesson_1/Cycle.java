9915012551da3eeb12d3cc3a5e4be7c250f755bd
MaksimBS <95247330+MaksimBS@users.noreply.github.com>
2021-12-08 08:16:29 +0300

    исправление по ДЗ

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,24 +1,26 @@
 public class Cycle {
     public static void main(String[] args) {
         //цикл For от 0 до 20
-        for (int i = 0; i < 21 ; i++ ) {
+        for (int i = 0; i < 21; i++) {
             System.out.println(i);
         }
+        
         //цикл while [6, -6] (шаг 2)
         int i = 6;
-        while ( i > -7 ) {
+        while (i > -7) {
             System.out.println(i);
-            i = i- 2;
+            i -= 2;
         }
+        
         //цикл do
         i = 10;
-        int symm = 0;
+        int sumOdd = 0;
         do {
-            if (i%2==1) {
-                symm = symm + i;
+            if (i % 2 == 1) {
+                sumOdd += i;
             }
             i++;
         } while (i < 21);
-        System.out.println(symm);
+        System.out.println(sumOdd);
     }
 }
