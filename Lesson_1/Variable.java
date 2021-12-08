e7387b4768aab3ae296a253e8e2979893e1e6bbc
MaksimBS <95247330+MaksimBS@users.noreply.github.com>
2021-12-08 08:00:10 +0300

    Update Variable.java
    исправление по ДЗ.

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -3,18 +3,25 @@ public class Variable {
         //Обьявление переменных
         byte totalCores = 4;
         System.out.println(totalCores);
+        
         short baseFrequency = 3500;
         System.out.println(baseFrequency);
+        
         int memorySize = 128;
         System.out.println(memorySize);
+        
         long tdp = 95;
         System.out.println(tdp);
-        float voltage = 220.1;
+        
+        float voltage = 220.1f;
         System.out.println(voltage);
+        
         double price = 450.99;
         System.out.println(price);
+        
         char os = 'W';
         System.out.println(os);
+        
         boolean active = true;
         System.out.println(active);
     }
