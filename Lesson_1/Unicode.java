0e354c566191761eb6436de20365186a36fc88b1
MaksimBS <95247330+MaksimBS@users.noreply.github.com>
2021-12-08 08:08:52 +0300

    Update Unicode.java
    исправление по проверке ДЗ

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,6 +1,6 @@
 public class Unicode {
     public static void main(String[] args) {
-        for (char i = 9398 ; i < 10179 ; i++ ) {
+        for (char i = 9398; i < 10179; i++) {
             System.out.println(i);
         }
     }
